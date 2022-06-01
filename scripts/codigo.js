bba93d510ed3541fa703e2e8685d5f512083a88f
SeninHg <samleninvasques@gmail.com>
2022-05-31 23:51:22 -0500

    =Reestructuré el orden de los archivos y carpetas para mayor control. Además creé un archivo style.css que contendrá los estilos de la página principal, mientras que la página de películas tiene su propio archivo de estilo

---
+++ scripts/codigo.js
@@ -0,0 +1,7 @@
+function coraline(){
+    var CV = document.getElementById("Coraline_Video");
+    var CP = document.getElementById("Coraline_Portada");
+    CV.style.display = "block";
+    CP.style.display = "none";
+    CV.play();
+}
